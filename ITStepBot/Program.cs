5d05deddb56a6451f589c0d965d9e3367253314a
Leonid <list.leonid@gmail.com>
2019-03-20 20:32:09 +0200

    INIT

---
+++ ITStepBot/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using Telegram.Bot;
+using System.Net;
+using System.Text;
+using System.IO;
+using System.Net.Http;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+using MyStatAPI;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ITStepBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Login
+            Graphics.ShowHello();
+
+            Console.Write("Username: ");
+            string username = Console.ReadLine();
+            Console.Write("Password: ");
+            string password = null;
+            while (true)
+            {
+                var key = System.Console.ReadKey(true);
+                if (key.Key == ConsoleKey.Enter)
+                    break;
+                if (key.Key == ConsoleKey.Backspace)
+                {
+                    if (password?.Length != 0)
+                        password = password?.Remove(password.Length - 1);
+                }
+                else
+                    password += key.KeyChar;
+            }
+            Console.Write(Environment.NewLine);
+            #endregion
+
+            Api myStat = new Api(username, password, Cities.Kyiv);
+            myStat.TryLogin();
+            
+            try
+            {
+                myStat.CollectDailyPoints();
+                //myStat.CollectDailyPoints();
+                //int counter = 0;
+                //foreach(var hwe in myStat.Homeworks)
+                //{
+                //    Console.WriteLine(++counter + " " + hwe.Id + " => " + hwe.Theme);
+                //}
+                //Console.WriteLine("Choose id >> ");
+                //int id = int.Parse(Console.ReadLine());
+                //Console.WriteLine("Sleeping... 2sec");
+                //Thread.Sleep(2000);
+                ////myStat.UploadHomeworkFile(37559, @"C:\Users\Leonid\Desktop\lisa.jpg");
+                ////myStat.DownloadHomeworkFile(myStat.Homeworks.Find(x => x.Id == id.ToString()));
+                Task.WaitAll();
+            } catch(Exception e)
+            {
+                Logger.Log(e.Message, ConsoleColor.Gray);
+            }
+            Task.WaitAll();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+## **MyStat API .NET Core** 
+**Author**: [Leonid List](https://t.me/leonidlist)
+
+MyStat is a unique student service designed and used at the STEP Computer Academy (https://itstep.org). Officially MyStat has Andoid/iOS application and Web Version. But I want to create API that all developers using C# can use it. In the future, a telegram bot will be implemented so that you can use all the functions in this beautiful messenger.
+
